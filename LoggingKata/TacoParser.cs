cfc6ae0affe1739aaadb17ea89a66d22ec80ab2e
Ford Henley <info@fordswebhelp.com>
2025-11-17 15:56:08 -0500

    changed conversion back to Parse for double

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -28,14 +28,14 @@ namespace LoggingKata
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
             var latitude = cells[0];
-            var latitudeDouble = Convert.ToDouble(latitude);
+            var latitudeDouble = double.Parse(latitude);
             
             
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
             var longitude = cells[1];
-            var longitudeDouble = Convert.ToDouble(longitude); 
+            var longitudeDouble = double.Parse(longitude); 
             
             
             // TODO: Grab the name from your array at index 2
